0b9f8621b6df7ed282460c928565675e6dfb7ef8
Today <76273733+today2098@users.noreply.github.com>
2024-09-14 18:41:15 +0900

    [fix] Fix the path to problem

--- test/aoj-DSL_2_G-lazy_segment_tree.test.cpp
+++ test/aoj-DSL_2_G-lazy_segment_tree.test.cpp
@@ -1,4 +1,4 @@
-#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_GG"
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/3/DSL/2/DSL_2_G"
 
 #include <iostream>
 #include <vector>
