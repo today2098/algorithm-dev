8f74b9c8f77f7492e743a8adfb294a0ee2b30255
Today <76273733+today2098@users.noreply.github.com>
2024-09-16 01:47:08 +0900

    [add] Test about two functions, "lcs()" and "lis()"

---
+++ test/yosupo-longest_increasing_subsequence-longest_increasing_subsequence.test.cpp
@@ -0,0 +1,29 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/longest_increasing_subsequence"
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include "../lib/Others/longest_increasing_subsequence.hpp"
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    for(auto &in : a) std::cin >> in;
+
+    auto &&dp = algorithm::lis(a);
+    auto itr = std::max_element(dp.begin(), dp.end()) - dp.begin();
+
+    std::vector<int> ans;
+    ans.push_back(itr);
+    for(int i = itr - 1; i >= 0; --i) {
+        if(dp[i] == dp[ans.back()] - 1) ans.push_back(i);
+    }
+    std::reverse(ans.begin(), ans.end());
+
+    std::cout << ans.size() << "\n";
+    for(auto elem : ans) std::cout << elem << " ";
+    std::cout << std::endl;
+}
