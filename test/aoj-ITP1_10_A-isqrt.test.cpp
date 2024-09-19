5d3abe78f2a934953b977483f396d3b20aebc77f
Today <76273733+today2098@users.noreply.github.com>
2024-09-20 05:12:40 +0900

    [add] Test about "isqrt()" and "nth_root()"

---
+++ test/aoj-ITP1_10_A-isqrt.test.cpp
@@ -0,0 +1,14 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/10/ITP1_10_A"
+
+#include <iostream>
+
+#include "../lib/Math/Algebra/square_root.hpp"
+
+int main() {
+    double x1, y1, x2, y2;
+    std::cin >> x1 >> y1 >> x2 >> y2;
+
+    auto tmp = (x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2);
+    auto &&ans = algorithm::isqrt(tmp, 1e-8);
+    printf("%.6f\n", ans);
+}
---
+++ test/aoj-ITP1_10_D-nth_root.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/10/ITP1_10_D"
+
+#include <algorithm>
+#include <cmath>
+#include <iostream>
+#include <vector>
+
+#include "../lib/Math/Algebra/nth_root.hpp"
+#include "../lib/Math/Algebra/power.hpp"
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> x(n), y(n);
+    for(auto &in : x) std::cin >> in;
+    for(auto &in : y) std::cin >> in;
+
+    auto f = [&](int p) -> double {
+        double tmp = 0.0;
+        for(int i = 0; i < n; ++i) tmp += algorithm::ipow(std::abs(x[i] - y[i]), p);
+        return algorithm::nth_root(tmp, p, 1e-10);
+    };
+
+    for(int p = 1; p <= 3; ++p) {
+        auto &&ans = f(p);
+        printf("%.8lf\n", ans);
+    }
+
+    int ans = 0;
+    for(int i = 0; i < n; ++i) ans = std::max(ans, std::abs(x[i] - y[i]));
+
+    printf("%.8lf\n", (double)ans);
+}
