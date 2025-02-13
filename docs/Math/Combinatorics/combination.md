af1c9854c28f291f79d2cc5a309e560af1fa3637
Today <76273733+today2098@users.noreply.github.com>
2025-02-13 22:44:55 +0900

    [update] Reimpl a class, "Combination" by singleton model

--- docs/Math/Combinatorics/combination.md
+++ docs/Math/Combinatorics/combination.md
@@ -1,11 +1,22 @@
+---
+title: 数え上げ（mod付き）
+documentation_of: //lib/Math/Combinatorics/combination.hpp
+---
+
+
 ## 概要
 
-特定の性質を備えた対象を，素数 $p$ を法として mod を取りながら数え上げる．
+ある場合の数を，素数 $p$ を法として mod を取りながら数え上げる．
 
-計算量は，前処理に $\mathcal{O}(N)$ ，クエリ処理に $\mathcal{O}(1)$ を要する．
+計算量は，前処理が $\mathcal{O}(N)$ ，各クエリ処理が $\mathcal{O}(1)$ となる．
 
 
 ## 参考文献
 
 1. drken1215. "よくやる二項係数 (nCk mod. p)、逆元 (a^-1 mod. p) の求め方". HatenaBlog. <https://drken1215.hatenablog.com/entry/2018/06/08/210000>.
-1. "競プロでよく使う二項係数(nCk)を素数(p)で割った余りの計算と逆元のまとめ". アルゴリズムロジック. <https://algo-logic.info/combination/>.
+2. "競プロでよく使う二項係数(nCk)を素数(p)で割った余りの計算と逆元のまとめ". アルゴリズムロジック. <https://algo-logic.info/combination/>.
+
+
+## 問題
+
+- "E - Max-Min Sum". AtCoder Beginner Contest 151. <https://atcoder.jp/contests/abc151/tasks/abc151_e>.
--- lib/Math/Combinatorics/combination.hpp
+++ lib/Math/Combinatorics/combination.hpp
@@ -1,74 +1,98 @@
 #ifndef ALGORITHM_COMBINATION_HPP
 #define ALGORITHM_COMBINATION_HPP 1
 
-/**
- * @brief 組合せ（mod付き）
- * @docs docs/Math/Combinatorics/combination.md
- */
-
 #include <cassert>
 #include <vector>
 
 namespace algorithm {
 
-// 組合せ（mod付き）．
+// 数え上げ（mod付き）．
 template <int mod>
 class Combination {
-    int m_mx;                       // m_mx:=(引数nの最大値).
-    std::vector<long long> m_fact;  // m_fact[n]:=(nの階乗).
-    std::vector<long long> m_inv;   // m_inv[n]:=(nの逆元).
-    std::vector<long long> m_finv;  // m_finv[n]:=(nの階乗の逆元).
+    static_assert(mod >= 2);
+
+    static int s_sz, s_cap;
+    static std::vector<long long> s_fact;  // s_fact[n]:=(nの階乗).
+    static std::vector<long long> s_inv;   // s_inv[n]:=(nの逆元).
+    static std::vector<long long> s_finv;  // s_finv[n]:=(nの階乗の逆元).
+
+    Combination() = default;
+
+    static void calc(int mx) {
+        assert(mx < mod);
+        if(mx < s_sz) return;
+        reserve(mx);
+        for(int n = s_sz; n <= mx; ++n) {
+            s_fact.push_back(s_fact[n - 1] * n % mod);
+            s_inv.push_back(mod - s_inv[mod % n] * (mod / n) % mod);
+            s_finv.push_back(s_finv[n - 1] * s_inv[n] % mod);
+        }
+        s_sz = mx + 1;
+    }
 
 public:
-    // constructor. 制約は，modが素数かつ mx<mod であること．O(N).
-    Combination() : Combination(51e4) {}
-    explicit Combination(int mx) : m_mx(mx), m_fact(mx + 1), m_inv(mx + 1), m_finv(mx + 1) {
-        static_assert(mod >= 1);
-        assert(1 <= m_mx and m_mx < mod);
-        m_fact[0] = m_fact[1] = 1;
-        m_inv[1] = 1;
-        m_finv[0] = m_finv[1] = 1;
-        for(int n = 2; n <= m_mx; ++n) {
-            m_fact[n] = m_fact[n - 1] * n % mod;
-            m_inv[n] = mod - m_inv[mod % n] * (mod / n) % mod;
-            m_finv[n] = m_finv[n - 1] * m_inv[n] % mod;
-        }
-    }
-
     static constexpr int modulus() { return mod; }
+    static void reserve(int mx) {
+        if(mx < s_cap) return;
+        s_cap = mx + 1;
+        s_fact.reserve(s_cap);
+        s_inv.reserve(s_cap);
+        s_finv.reserve(s_cap);
+    }
     // 階乗．O(1).
-    long long factorial(int n) const {
-        assert(0 <= n and n <= m_mx);
-        return m_fact[n];
+    static long long factorial(int n) {
+        assert(0 <= n);
+        calc(n);
+        return s_fact[n];
     }
     // 逆元．O(1).
-    long long inverse(int n) const {
-        assert(1 <= n and n <= m_mx);
-        return m_inv[n];
+    static long long inverse(int n) {
+        assert(1 <= n);
+        calc(n);
+        return s_inv[n];
     }
     // 階乗の逆元．O(1).
-    long long inverse_fact(int n) const {
-        assert(0 <= n and n <= m_mx);
-        return m_finv[n];
+    static long long inverse_fact(int n) {
+        assert(0 <= n);
+        calc(n);
+        return s_finv[n];
     }
     // 順列．O(1).
-    long long nPk(int n, int k) const {
-        assert(0 <= k and k <= n and n <= m_mx);
-        return m_fact[n] * m_finv[n - k] % mod;
+    static long long nPk(int n, int k) {
+        assert(0 <= k and k <= n);
+        calc(n);
+        return s_fact[n] * s_finv[n - k] % mod;
     }
     // 組合せ．O(1).
-    long long nCk(int n, int k) const {
-        assert(0 <= n and n <= m_mx);
-        assert(k >= 0);
+    static long long nCk(int n, int k) {
+        assert(0 <= n);
+        assert(0 <= k);
         if(k > n) return 0;
-        return m_fact[n] * m_finv[n - k] % mod * m_finv[k] % mod;
+        calc(n);
+        return s_fact[n] * s_finv[n - k] % mod * s_finv[k] % mod;
     }
     // 重複組合せ．O(1).
-    long long nHk(int n, int k) const {
-        assert(n >= 1 and k >= 0);
+    static long long nHk(int n, int k) {
+        assert(1 <= n);
+        assert(0 <= k);
         return nCk(k + n - 1, k);
     }
 };
+
+template <int mod>
+int Combination<mod>::s_sz = 2;
+
+template <int mod>
+int Combination<mod>::s_cap = 2;
+
+template <int mod>
+std::vector<long long> Combination<mod>::s_fact({1, 1});
+
+template <int mod>
+std::vector<long long> Combination<mod>::s_inv({-1, 1});
+
+template <int mod>
+std::vector<long long> Combination<mod>::s_finv({1, 1});
 
 using Combination998244353 = Combination<998'244'353>;
 using Combination1000000007 = Combination<1'000'000'007>;
