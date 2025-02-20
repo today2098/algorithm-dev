30221bdc14e3e79d8aa78016be8d6966b1324404
Today <76273733+today2098@users.noreply.github.com>
2025-02-20 21:37:57 +0900

    [update] Reimpl a class, "PascalTriangle" by singleton pattern

--- lib/Math/Combinatorics/pascal_triangle.hpp
+++ lib/Math/Combinatorics/pascal_triangle.hpp
@@ -1,49 +1,48 @@
 #ifndef ALGORITHM_PASCAL_TRIANGLE_HPP
 #define ALGORITHM_PASCAL_TRIANGLE_HPP 1
 
-/**
- * @brief Pascal's Triangle（パスカルの三角形）
- * @docs docs/Math/Combinatorics/pascal_triangle.md
- */
-
-#include <algorithm>
 #include <cassert>
 #include <vector>
 
 namespace algorithm {
 
-template <typename Type = long long>
+template <typename T = long long>
 class PascalTriangle {
-public:
-    int m_mx;                             // m_mx:=(引数nの最大値).
-    std::vector<std::vector<Type> > m_c;  // m_c[n][k]:=(C(n,k) and C(n,n-k)).
+    static std::vector<std::vector<T> > s_c;  // s_c[n][k]:=(C(n,n-k) and C(n,k)).
+
+    PascalTriangle() = default;
+    ~PascalTriangle() = default;
 
 public:
-    // constructor. O(N^2).
-    PascalTriangle() : PascalTriangle(51e2) {}
-    explicit PascalTriangle(int mx) : m_mx(mx), m_c(mx + 1) {
-        assert(mx >= 0);
-        for(int n = 0; n <= m_mx; ++n) {
-            const int len = (n + 2) / 2;
-            m_c[n].resize(len);
-            m_c[n][0] = 1;
-            for(int k = 1; k < len; ++k) m_c[n][k] = nCk(n - 1, k - 1) + nCk(n - 1, k);
-        }
-    }
-
-    // 組合せ．O(1).
-    Type nCk(int n, int k) const {
-        assert(0 <= n and n <= m_mx);
+    // 組合せ．
+    static T nCk(int n, int k) {
+        assert(n >= 0);
         assert(k >= 0);
         if(k > n) return 0;
-        return (k <= n - k ? m_c[n][k] : m_c[n][n - k]);
+        resize(n + 1);
+        return (n - k < k ? s_c[n][n - k] : s_c[n][k]);
     }
-    // 重複組合せ．O(1).
-    Type nHk(int n, int k) const {
-        assert(n >= 1 and k >= 0);
+    // 重複組合せ．
+    static T nHk(int n, int k) {
+        assert(n >= 1);
+        assert(k >= 0);
         return nCk(k + n - 1, k);
     }
+    static void resize(int sz) {
+        const int now = s_c.size();
+        s_c.resize(sz);
+        for(int n = now; n < sz; ++n) {
+            const int len = (n + 2) / 2;
+            s_c[n].resize(len);
+            s_c[n][0] = 1;
+            for(int k = 1; k < len; ++k) s_c[n][k] = s_c[n - 1][k - 1] + (n - 1 - k < k ? s_c[n - 1][n - 1 - k] : s_c[n - 1][k]);
+        }
+    }
+    static void clear() { s_c.clear(); }
 };
+
+template <typename T>
+std::vector<std::vector<T> > PascalTriangle<T>::s_c;
 
 }  // namespace algorithm
 
