e20c27b82722d25ce6fae0fca9b5f0ccc29e5a9c
Today <76273733+today2098@users.noreply.github.com>
2025-03-02 18:26:49 +0900

    [clean] Refactor a class, "Sieve"

--- lib/Math/NumberTheory/sieve.hpp
+++ lib/Math/NumberTheory/sieve.hpp
@@ -12,20 +12,19 @@ namespace algorithm {
 // Sieve of Eratosthenes（エラトステネスの篩）.
 class Sieve {
     int m_sz;
-    // m_lpf[n]:=(自然数nの最小素因数). Least prime factor. m_lpf[n]==n のとき，nは素数．
+    // m_lpf[i]:=(正の奇数2*i+1の最小素因数). Least prime factor. m_lpf[i]==2*i+1 のとき，2*i+1は素数．
     std::vector<int> m_lpf;
 
 public:
     // constructor. n未満の自然数を篩にかける．O(N*loglogN).
     Sieve() : Sieve(0) {}
-    explicit Sieve(int n) : m_sz(n), m_lpf(n, -1) {
+    explicit Sieve(int n) : m_sz(n), m_lpf(n / 2, -1) {
         assert(n >= 0);
-        if(m_sz <= 2) return;
-        std::iota(m_lpf.begin() + 2, m_lpf.end(), 2);
-        for(int p = 2; p * p < m_sz; ++p) {
-            if(m_lpf[p] == p) {
-                for(int q = p * p; q < m_sz; q += p) {
-                    if(m_lpf[q] == q) m_lpf[q] = p;
+        for(long long p = 3; p < m_sz; p += 2) {
+            if(m_lpf[p / 2] == -1) {
+                m_lpf[p / 2] = p;
+                for(long long q = p * p; q < m_sz; q += p) {
+                    if(m_lpf[q / 2] == -1) m_lpf[q / 2] = p;
                 }
             }
         }
@@ -34,36 +33,23 @@ public:
     // 素数判定．O(1).
     bool is_prime(int n) const {
         assert(0 <= n and n < m_sz);
-        return m_lpf[n] == n;
+        if(n == 2) return true;
+        if(n % 2 == 0) return false;
+        return m_lpf[n / 2] == n;
     }
     // 自然数nの最小素因数を返す．O(1).
     int lpf(int n) const {
         assert(0 <= n and n < m_sz);
-        return m_lpf[n];
+        if(n < 2) return -1;
+        if(n % 2 == 0) return 2;
+        return m_lpf[n / 2];
     }
     // 高速素因数分解．O(logN).
     std::map<int, int> prime_factorize(int n) const {
         assert(1 <= n and n < m_sz);
         std::map<int, int> res;
-        while(n > 1) {
-            res[m_lpf[n]]++;
-            n /= m_lpf[n];
-        }
-        return res;
-    }
-    // 高速約数列挙．
-    std::vector<int> divisors(int n) const {
-        assert(1 <= n and n < m_sz);
-        std::vector<int> res({1});
-        for(const auto &[p, cnt] : prime_factorize(n)) {
-            const int sz = res.size();
-            int b = 1;
-            for(int i = 0; i < cnt; ++i) {
-                b *= p;
-                for(int j = 0; j < sz; ++j) res.push_back(res[j] * b);
-            }
-        }
-        std::sort(res.begin(), res.end());
+        for(; n % 2 == 0; n /= 2) res[2]++;
+        for(; n > 1; n /= m_lpf[n / 2]) res[m_lpf[n / 2]]++;
         return res;
     }
     // オイラーのファイ関数．n以下でnと互いに素な自然数の個数を求める．O(logN).
@@ -77,7 +63,7 @@ public:
     std::vector<int> mobius() const {
         std::vector<int> res(m_sz, 1);  // res[n]:=μ(n).
         for(int p = 2; p < m_sz; ++p) {
-            if(m_lpf[p] == p) {
+            if(lpf(p) == p) {
                 res[p] = -1;
                 for(int q = 2 * p; q < m_sz; q += p) {
                     if((q / p) % p == 0) res[q] = 0;  // nがある素数pで2回以上割り切れるとき，μ(n)=0.
