1439da63eb3e4e393cbf87e2e29f687787e43247
Today <76273733+today2098@users.noreply.github.com>
2025-03-23 13:39:36 +0900

    [update] Print the elapsed time by microsecond on debug

--- docs/Utils/debug.md
+++ docs/Utils/debug.md
@@ -1,23 +1,29 @@
+---
+title: デバッグ用関数形式マクロ
+documentation_of: //lib/Utils/debug.hpp
+---
+
+
 ## 概要
 
 ローカル環境でのみ実行されるデバッグ用の関数形式マクロ．
-引数にあるものを，名前と行番号とを合わせて，標準エラー出力 (`std::clog`) に出力する．
+
+引数にあるものを，経過時間（マイクロ秒）と行番号，引数名を合わせて，標準エラー出力 (`std::clog`) に出力する．
 可変長引数に対応しており，複数の値を同時に出力することもできる．
 
 引数として使用できるものは次の通り．
 
 - リテラル値
 - スカラー型の変数
-- `std::string`, `std::string_view`, `std::pair`, `std::tuple`, `std::stack`, `std::queue`, `std::priority_queue` のオブジェクト
-- iterator が実装されているクラス（STL のコンテナクラスなど）のオブジェクト
+- `std::string`, `std::string_view`, `std::stack`, `std::queue`, `std::priority_queue`, `std::pair`, `std::tuple` のオブジェクト
+- `const_iterator` が実装されているクラス（STL のコンテナクラスなど）のオブジェクト
 - 出力演算子が定義されているクラスのオブジェクト
 
 使用する際は，コンパイル時に「`-D=DEBUG`」とオプション指定する．
 
 使用例は次の通り．
-引数は1個以上指定すること．
 
-```main.cpp
+```cpp
 int a = 0;
 double b = 3.14;
 std::string s = "Hello, world!";
@@ -29,23 +35,25 @@ debug(a, b);
 debug(s);
 debug(p);
 debug(v);
-// debug();  // error!
+debug();
 ```
 
 ```bash
-$ g++ -std=gnu++20 -D=DEBUG -o debug.out main.cpp
-$ ./debug
-[L50] ('i', -1LL): (i, -1)
-[L51] (a, b): (0, 3.14)
-[L52] s: Hello, world!
-[L53] p: {1, 1.41}
-[L54] v: [1 1 2 3 5]
+$ g++ -std=gnu++23 -D=DEBUG -o debug.out main.cpp
+$ ./debug.out
+(       0) [L61] ('i', -1LL): (i, -1)
+(     119) [L62] (a, b): (0, 3.14)
+(     164) [L63] s: Hello, world!
+(     198) [L64] p: {1, 1.41}
+(     226) [L65] v: [1 1 2 3 5]
+(     270) [L66] (empty)
 ```
 
 
 ## 参考文献
 
 1. "SFINAE". Wikipedia. <https://ja.wikipedia.org/wiki/SFINAE>.
+1. "任意の式によるSFINAE [N2634]". cpprefjp. <https://cpprefjp.github.io/index.html>.
 1. _EnumHack. "C++メタ関数のまとめ". Qiita. <https://qiita.com/_EnumHack/items/ee2141ad47915c55d9cb>.
 1. terukazu. "特定のメンバ関数有無で、呼び出す関数を変えたい". Qiita. <https://qiita.com/terukazu/items/e257c05a7b191d32c577>.
 1. "競技プログラミングで print デバッグ". <https://naskya.net/post/0002/>.
--- lib/Utils/debug.hpp
+++ lib/Utils/debug.hpp
@@ -1,11 +1,8 @@
 #ifndef ALGORITHM_DEBUG_HPP
 #define ALGORITHM_DEBUG_HPP 1
 
-/**
- * @brief デバッグ用関数形式マクロ
- * @docs docs/Utils/debug.md
- */
-
+#include <chrono>
+#include <iomanip>
 #include <iostream>
 #include <iterator>
 #include <queue>
@@ -16,11 +13,9 @@
 #include <type_traits>
 #include <utility>
 
-// #define DEBUG
-
 #ifdef DEBUG
 
-#define debug(...) algorithm::debug::debug_internal(__LINE__, #__VA_ARGS__, __VA_ARGS__)
+#define debug(...) algorithm::debug::debug_internal(__LINE__ __VA_OPT__(, #__VA_ARGS__, __VA_ARGS__))
 
 namespace algorithm {
 
@@ -28,42 +23,127 @@ namespace debug {
 
 constexpr std::ostream &os = std::clog;
 
-struct has_iterator_impl {
+struct has_const_iterator_impl {
     template <class T>
-    static constexpr std::true_type check(typename T::iterator *);
+    static constexpr std::true_type check(typename T::const_iterator *);
 
     template <class T>
     static constexpr std::false_type check(...);
 };
 
 template <class T>
-class has_iterator : public decltype(has_iterator_impl::check<T>(nullptr)) {};
+class has_const_iterator : public decltype(has_const_iterator_impl::check<T>(nullptr)) {};
 
 // Prototype declaration.
-void print(const std::string &s);
-void print(std::string_view s);
+template <typename Type>
+auto print(const Type &) -> typename std::enable_if<!has_const_iterator<Type>::value>::type;
+
+template <class Container>
+auto print(const Container &) -> typename std::enable_if<has_const_iterator<Container>::value>::type;
+
+void print(const std::string &);
+
+void print(std::string_view);
+
+template <typename... Types>
+void print(const std::stack<Types...> &);
+
+template <typename... Types>
+void print(const std::queue<Types...> &);
+
+template <typename... Types>
+void print(const std::priority_queue<Types...> &);
+
 template <typename T, typename U>
-void print(const std::pair<T, U> &p);
-template <class T, std::size_t... Idxes>
-void print_tuple(const T &t, std::index_sequence<Idxes...>);
-template <typename... Ts>
-void print(const std::tuple<Ts...> &t);
-template <typename... Ts>
-void print(const std::stack<Ts...> &st);
-template <typename... Ts>
-void print(const std::queue<Ts...> &que);
-template <typename... Ts>
-void print(const std::priority_queue<Ts...> &pque);
-template <class T, typename std::enable_if_t<has_iterator<T>::value, bool> = false>
-void print(const T &v);
-template <typename T, typename std::enable_if_t<!has_iterator<T>::value, bool> = false>
-void print(const T &elem);
-template <typename T, typename... Args>
-void debug_internal(int line, std::string_view context, T &&first, Args &&...args);
+void print(const std::pair<T, U> &);
 
-void print(const std::string &s) { os << s; }
+template <typename... Types>
+void print(const std::tuple<Types...> &);
 
-void print(std::string_view s) { os << s; }
+template <class Tuple, std::size_t... Idxes>
+void print_tuple(const Tuple &, std::index_sequence<Idxes...>);
+
+// Implementation.
+void elapsed() {
+    static const auto start = std::chrono::system_clock::now();
+    auto t = std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::system_clock::now() - start).count();
+    os << "(" << std::setw(8) << t << ")";
+}
+
+template <typename Type, typename... Args>
+void debug_internal(int line, std::string_view context, const Type &first, const Args &...args) {
+    constexpr const char *open_bracket = (sizeof...(args) == 0 ? "" : "(");
+    constexpr const char *close_bracket = (sizeof...(args) == 0 ? "" : ")");
+    elapsed();
+    os << " [L" << line << "] " << open_bracket << context << close_bracket << ": " << open_bracket;
+    print(first);
+    ((os << ", ", print(args)), ...);
+    os << close_bracket << std::endl;
+}
+
+void debug_internal(int line) {
+    elapsed();
+    os << " [L" << line << "] (empty)" << std::endl;
+}
+
+template <typename Type>
+auto print(const Type &a) -> typename std::enable_if<!has_const_iterator<Type>::value>::type {
+    os << a;
+}
+
+template <class Container>
+auto print(const Container &c) -> typename std::enable_if<has_const_iterator<Container>::value>::type {
+    os << "[";
+    for(auto iter = std::cbegin(c); iter != std::cend(c); ++iter) {
+        if(iter != std::cbegin(c)) os << " ";
+        print(*iter);
+    }
+    os << "]";
+}
+
+void print(const std::string &s) {
+    os << s;
+}
+
+void print(std::string_view sv) {
+    os << sv;
+}
+
+template <typename... Types>
+void print(const std::stack<Types...> &st) {
+    std::stack<Types...> tmp = st;
+    os << "[";
+    while(!tmp.empty()) {
+        print(tmp.top());
+        tmp.pop();
+        if(!tmp.empty()) os << " ";
+    }
+    os << "]";
+}
+
+template <typename... Types>
+void print(const std::queue<Types...> &que) {
+    std::queue<Types...> tmp = que;
+    os << "[";
+    while(!tmp.empty()) {
+        print(tmp.front());
+        tmp.pop();
+        if(!tmp.empty()) os << " ";
+    }
+    os << "]";
+}
+
+template <typename... Types>
+void print(const std::priority_queue<Types...> &pque) {
+    std::priority_queue<Types...> tmp = pque;
+    os << "[";
+    while(!tmp.empty()) {
+        print(tmp.top());
+        tmp.pop();
+        if(!tmp.empty()) os << " ";
+    }
+    os << "]";
+}
 
 template <typename T, typename U>
 void print(const std::pair<T, U> &p) {
@@ -74,75 +154,16 @@ void print(const std::pair<T, U> &p) {
     os << "}";
 }
 
-template <class T, std::size_t... Idxes>
-void print_tuple(const T &t, std::index_sequence<Idxes...>) {
+template <typename... Types>
+void print(const std::tuple<Types...> &t) {
+    print_tuple(t, std::make_index_sequence<sizeof...(Types)>());
+}
+
+template <class Tuple, std::size_t... Idxes>
+void print_tuple(const Tuple &t, std::index_sequence<Idxes...>) {
     os << "{";
     ((os << (Idxes == 0 ? "" : ", "), print(std::get<Idxes>(t))), ...);
     os << "}";
-}
-
-template <typename... Ts>
-void print(const std::tuple<Ts...> &t) {
-    print_tuple(t, std::make_index_sequence<sizeof...(Ts)>());
-}
-
-template <typename... Ts>
-void print(const std::stack<Ts...> &st) {
-    std::stack<Ts...> tmp = st;
-    os << "[";
-    while(!tmp.empty()) {
-        print(tmp.top());
-        tmp.pop();
-        if(!tmp.empty()) os << " ";
-    }
-    os << "]";
-}
-
-template <typename... Ts>
-void print(const std::queue<Ts...> &que) {
-    std::queue<Ts...> tmp = que;
-    os << "[";
-    while(!tmp.empty()) {
-        print(tmp.front());
-        tmp.pop();
-        if(!tmp.empty()) os << " ";
-    }
-    os << "]";
-}
-
-template <typename... Ts>
-void print(const std::priority_queue<Ts...> &pque) {
-    std::priority_queue<Ts...> tmp = pque;
-    os << "[";
-    while(!tmp.empty()) {
-        print(tmp.top());
-        tmp.pop();
-        if(!tmp.empty()) os << " ";
-    }
-    os << "]";
-}
-
-template <class T, typename std::enable_if_t<has_iterator<T>::value, bool> >
-void print(const T &v) {
-    os << "[";
-    for(auto itr = std::begin(v); itr != std::end(v); ++itr) {
-        if(itr != std::begin(v)) os << " ";
-        print(*itr);
-    }
-    os << "]";
-}
-
-template <typename T, typename std::enable_if_t<!has_iterator<T>::value, bool> >
-void print(const T &elem) { os << elem; }
-
-template <typename T, typename... Args>
-void debug_internal(int line, std::string_view context, T &&first, Args &&...args) {
-    constexpr const char *open_bracket = (sizeof...(args) == 0 ? "" : "(");
-    constexpr const char *close_bracket = (sizeof...(args) == 0 ? "" : ")");
-    os << "[L" << line << "] " << open_bracket << context << close_bracket << ": " << open_bracket;
-    print(std::forward<T>(first));
-    ((os << ", ", print(std::forward<Args>(args))), ...);
-    os << close_bracket << std::endl;
 }
 
 }  // namespace debug
