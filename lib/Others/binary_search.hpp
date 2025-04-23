3d8621b7e8e40eef6d86e9a2a656a97e9485fbfb
Today <76273733+today2098@users.noreply.github.com>
2025-04-23 17:04:25 +0900

    [update] Update a function, "bisearch()"

--- lib/Others/binary_search.hpp
+++ lib/Others/binary_search.hpp
@@ -1,28 +1,34 @@
 #ifndef ALGORITHM_BINARY_SEARCH_HPP
 #define ALGORITHM_BINARY_SEARCH_HPP 1
 
+#include <cassert>
 #include <concepts>
 #include <functional>
+#include <numeric>
 #include <type_traits>
 
 namespace algorithm {
 
-template <std::integral Type, class Eval>
-constexpr Type bisearch(Type ok, Type ng, Eval eval) {
-    static_assert(std::is_convertible_v<Eval, std::function<bool(Type)>>);
+template <std::integral Type, class Pred>
+constexpr Type bisearch(Type ok, Type ng, Pred pred) {
+    static_assert(std::is_convertible_v<Pred, std::function<bool(Type)>>);
+    assert(ok <= ng);
+    if(!pred(ok)) return ok;
     while(ng - ok > 1) {
-        Type mid = ok + (ng - ok) / 2;
-        (eval(mid) ? ok : ng) = mid;
+        Type mid = std::midpoint(ok, ng);
+        (pred(mid) ? ok : ng) = mid;
     }
     return ng;
 }
 
-template <std::floating_point Type, class Eval>
-constexpr Type bisearch(Type ok, Type ng, Type eps, Eval eval) {
-    static_assert(std::is_convertible_v<Eval, std::function<bool(Type)>>);
+template <std::floating_point Type, class Pred>
+constexpr Type bisearch(Type ok, Type ng, Type eps, Pred pred) {
+    static_assert(std::is_convertible_v<Pred, std::function<bool(Type)>>);
+    assert(ok <= ng);
+    if(!pred(ok)) return ok;
     while(ng - ok > eps) {
-        Type mid = ok + (ng - ok) / 2;
-        (eval(mid) ? ok : ng) = mid;
+        Type mid = std::midpoint(ok, ng);
+        (pred(mid) ? ok : ng) = mid;
     }
     return ng;
 }
