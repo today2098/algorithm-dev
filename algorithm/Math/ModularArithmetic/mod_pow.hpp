82657960f425f43c03d4d65bf2c05e52b2fc42aa
Today <76273733+today2098@users.noreply.github.com>
2025-08-31 12:12:17 +0000

    [clean] Refactor a func, "mod_pow()"

--- algorithm/Math/ModularArithmetic/mod_pow.hpp
+++ algorithm/Math/ModularArithmetic/mod_pow.hpp
@@ -2,23 +2,39 @@
 #define ALGORITHM_MOD_POW_HPP 1
 
 #include <cassert>
+#include <concepts>
+#include <cstdint>
 
 #include "mod_inv.hpp"
+#include "modulo.hpp"
 
 namespace algorithm {
 
-// 繰り返し二乗法（mod付き）．O(log k).
-constexpr long long mod_pow(long long n, long long k, int mod) {
-    assert(mod >= 1);
-    if(n < -mod or mod <= n) n %= mod;
-    if(n < 0) n += mod;
-    if(k < 0) n = mod_inv(n, mod), k = -k;
-    long long res = 1;
+namespace internal {
+
+// return n^k mod m.
+constexpr std::uint32_t mod_pow(std::uint64_t n, unsigned long long k, std::uint32_t mod) {
+    std::uint64_t res = 1;
     for(; k > 0; k >>= 1) {
-        if(k & 1LL) res = res * n % mod;
+        if(k & 1ULL) res = res * n % mod;
         n = n * n % mod;
     }
     return res;
+}
+
+}  // namespace internal
+
+// 繰り返し二乗法（mod付き）．O(log k).
+template <std::integral Type>
+constexpr std::int64_t mod_pow(Type n, long long k, std::int32_t mod) {
+    assert(mod >= 1);
+    auto r = ::algorithm::internal::modulo(n, mod);
+    if(k < 0) {
+        auto [x, g] = ::algorithm::internal::mod_inv(r, mod);
+        assert(g == 1);
+        r = x, k = -k;
+    }
+    return internal::mod_pow(r, k, mod);
 }
 
 }  // namespace algorithm
--- docs/Math/ModularArithmetic/mod_pow.md
+++ docs/Math/ModularArithmetic/mod_pow.md
@@ -6,16 +6,9 @@ documentation_of: //algorithm/Math/ModularArithmetic/mod_pow.hpp
 
 ## 概要
 
-自然数 $n, k, m$ に対して，
-
-$$
-n^k \bmod m
-$$
-
-を求める．
+$n, k \in \mathbb{Z}, m \in \mathbb{N}$ について，$n^k \bmod m$ を求める．
 
 本実装では「繰り返し二乗法」を用いており，計算量は $\mathcal{O}(\log k)$ である．
-
 
 ## 問題
 
